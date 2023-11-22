76f9fd9279fa021c1905df84c5eb2ee11a63a611
Andrezepf <andrezepf@gmail.com>
2023-11-22 13:10:55 -0300

    menus com login apenas

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,27 +7,31 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./tabela_up.php">Usuários</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./tabela_ep.php">Empresas</a>
-        </li>
-        <?php
+      <?php
         if(!isset($_SESSION)){
           session_start();
+        }
+        if(!isset($_SESSION['nome'])){
+          ?>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+          </li>
+        <?php
         }
         if(isset($_SESSION['nome'])){
           ?>
           <li class="nav-item">
-          <a class="nav-link" href="./logout.php">Logout</a>
-        </li>
+            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./tabela_up.php">Usuários</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./tabela_ep.php">Empresas</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="./logout.php">Logout</a>
+          </li>
           <?php
       }
       ?>
