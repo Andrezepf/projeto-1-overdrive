33572f8da70da3b07d0f29b7a5d9b120b3b5fffb
Andrezepf <andrezepf@gmail.com>
2023-12-05 12:30:05 -0300

    getters e setters

--- model/empresa.php
+++ model/empresa.php
@@ -23,6 +23,76 @@ class Empresa{
 
 
 
+    public function getCnpj(){
+        return $this->cnpj;
+    }
+
+
+    public function setCnpj($cnpj){
+        $this->cnpj = $cnpj;
+
+        return $this;
+    }
+
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+
+    public function setNome($nome){
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+
+    public function getNome_fantasia(){
+        return $this->nome_fantasia;
+    }
+
+
+    public function setNome_fantasia($nome_fantasia){
+        $this->nome_fantasia = $nome_fantasia;
+
+        return $this;
+    }
+
+
+    public function getEndereco(){
+        return $this->endereco;
+    }
+
+
+    public function setEndereco($endereco){
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+
+    public function getTelefone(){
+        return $this->telefone;
+    }
+
+
+    public function setTelefone($telefone){
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+
+    public function getResponsavel(){
+        return $this->responsavel;
+    }
+
+
+    public function setResponsavel($responsavel){
+        $this->responsavel = $responsavel;
+
+        return $this;
+    }
 }
 
 
--- model/usuario.php
+++ model/usuario.php
@@ -30,6 +30,112 @@ class Usuario{
 
 
 
+    public function getNome(){
+        return $this->nome;
+    }
+
+   
+    public function setNome($nome){
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    
+    public function getCpf(){
+        return $this->cpf;
+    }
+
+ 
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+
+    public function getCnh(){
+        return $this->cnh;
+    }
+
+
+    public function setCnh($cnh){
+        $this->cnh = $cnh;
+
+        return $this;
+    }
+
+
+    public function getTelefone(){
+        return $this->telefone;
+    }
+
+ 
+    public function setTelefone($telefone){
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+
+    public function getEndereco(){
+        return $this->endereco;
+    }
+
+
+    public function setEndereco($endereco){
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+
+    public function getCarro(){
+        return $this->carro;
+    }
+
+
+    public function setCarro($carro){
+        $this->carro = $carro;
+
+        return $this;
+    }
+
+
+    public function getEmpresa(){
+        return $this->empresa;
+    }
+
+
+    public function setEmpresa($empresa){
+        $this->empresa = $empresa;
+
+        return $this;
+    }
+
+
+    public function getSenha(){
+        return $this->senha;
+    }
+
+
+    public function setSenha($senha){
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+
+    public function getAcesso(){
+        return $this->acesso;
+    }
+
+ 
+    public function setAcesso($acesso){
+        $this->acesso = $acesso;
+
+        return $this;
+    }
 }
 
 
